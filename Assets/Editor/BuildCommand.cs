5cc8b49bed1c0849049e384cb263767f6fb10256
Masato Urai <masato.uraway@gmail.com>
2020-02-18 21:18:56 +0800

    initial commit

---
+++ Assets/Editor/BuildCommand.cs
@@ -0,0 +1,39 @@
+using System.Linq;
+using System.IO;
+using System.Collections.Generic;
+using UnityEditor;
+
+public static class BuildHelper
+{
+	private static string _buildLocation;
+
+	public static void StandaloneWindows64()
+	{
+		SetupVariables();
+		BuildPipeline.BuildPlayer(GetScenes(), _buildLocation + ".exe", BuildTarget.StandaloneWindows64, BuildOptions.None);
+	}
+
+	public static void StandaloneLinux64()
+	{
+		SetupVariables();
+		BuildPipeline.BuildPlayer(GetScenes(), _buildLocation, BuildTarget.StandaloneLinuxUniversal, BuildOptions.None);
+	}
+
+	public static void StandaloneOSX()
+	{
+		SetupVariables();
+		BuildPipeline.BuildPlayer(GetScenes(), _buildLocation + ".app", BuildTarget.StandaloneOSX, BuildOptions.None);
+	}
+
+	private static string[] GetScenes()
+	{
+		return EditorBuildSettings.scenes.Where(s => s.enabled).Select(s => s.path).ToArray();
+	}
+
+	private static void SetupVariables()
+	{
+		PlayerSettings.productName = "Unity Sample";
+		PlayerSettings.bundleVersion = "0.0.0.0";
+		_buildLocation = "./Builds/";
+	}
+}
